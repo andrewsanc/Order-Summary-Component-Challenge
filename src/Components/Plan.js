47d85f06b22c7c77aad3843a2e7127905e268a83
TB831 <druu831@gmail.com>
2021-10-21 10:50:54 -0700

    Plan component boilerplate

---
+++ src/Components/Plan.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Plan.css";
+
+const Plan = () => {
+  return (
+    <div>
+      <div>Plan Selection Icon</div>
+      <div>
+        <div>Plan Selection Title</div>
+        <div>Plan Selection Price</div>
+      </div>
+      <div>Change Plan Link</div>
+    </div>
+  );
+};
+
+export default Plan;
--- src/Components/SummaryCard.js
+++ src/Components/SummaryCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SummaryCard.css";
 import illustrationHero from "../images/illustration-hero.svg";
+import Plan from "./Plan";
 
 const SummaryCard = () => {
   return (
@@ -10,14 +11,7 @@ const SummaryCard = () => {
         <div>Order Summary</div>
         <div>Order Description</div>
       </div>
-      <div>
-        <div>Plan Selection Icon</div>
-        <div>
-          <div>Plan Selection Title</div>
-          <div>Plan Selection Price</div>
-        </div>
-        <div>Change Plan Link</div>
-      </div>
+      <Plan />
       <div className='buttons flex-col'>
         <button className='button primary'>Proceed to Payment</button>
         <button className='button'>Cancel Order</button>
